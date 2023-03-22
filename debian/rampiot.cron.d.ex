695feb180ba531ea043e4202a5f28bc2e96e0c97
Kari Kolehmainen <kari.kolehmainen@vtt.fi>
2023-03-22 11:16:27 +0200

    removed debian package that was outdates

--- debian/rampiot.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the rampiot package
-#
-0 4	* * *	root	[ -x /usr/bin/rampiot_maintenance ] && /usr/bin/rampiot_maintenance
--- debian/rampiot/opt/rampiot/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $(docker-compose version) == *" 1.29."* ]]; then
-#if [[ $(docker -v) == *" 20.10."* ]]; then
-    echo 'Docker version 20.10!'
-else 
-    echo 'Docker version not 20.10!'
-fi
